bad09d0ecf255fdb6c5144adf091c7bfb70325d7
CHOPPER\PC <duytran1402@gmail.com>
2018-09-19 23:16:06 +0700

    (+) landing page

---
+++ Web/Controllers/TransactionController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+
+using Data.Models;
+using System.Web.Mvc;
+using Web.Helpers;
+using Web.Infrastructure;
+
+namespace Web.Controllers
+{
+    public class TransactionController : Controller
+    {
+        #region Fields
+
+        private readonly UserManager _userManager;
+        #endregion
+
+        #region Constructor
+
+        public TransactionController(UserManager userManager)
+        {
+            _userManager = userManager;
+        }
+
+        #endregion
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
